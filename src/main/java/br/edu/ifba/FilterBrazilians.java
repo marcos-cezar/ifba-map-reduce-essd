6c1104705988b53ed28a1a734a51a47779bfee5c
Marcos Cezar <marcos.cezar.mendes@gmail.com>
2014-02-18 14:54:21 -0300

    Adicionado ao código uma maneira de testar fora do ambiente do hadoop.

--- src/main/java/br/edu/ifba/FilterBrazilians.java
+++ src/main/java/br/edu/ifba/FilterBrazilians.java
@@ -1,21 +1,53 @@
 package br.edu.ifba;
 
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapOutputCollector;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
-public class FilterBrazilians {
+
+public class FilterBrazilians extends Configured implements Tool {
 
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
         //TODO implementar as chamadas principais.
+        FilterBrazilians filterBrazilians = new FilterBrazilians();
+        int exitCode = ToolRunner.run(filterBrazilians, args);
+
+        System.exit(exitCode);
+
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            System.err.println("O Programa deve ser usado assim: FilterBrazilians <in> <out>");
+            System.exit(2);
+        }
+
+        Job job = new Job(getConf());
+        job.setJobName(this.getClass().getName());
+        job.setJarByClass(FilterBrazilians.class);
+        job.setMapperClass(FilterBraziliansMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean success = job.waitForCompletion(true);
+
+        return success ? 0 : 1;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
 
@@ -34,6 +66,7 @@ public class FilterBrazilians {
 
             String linha = value.toString();
 
+            System.out.println("O que tem na linha: " + linha);
 
             if (value.toString().toLowerCase().matches(regexFilter)) {
                 context.write(NullWritable.get(), value.toString());
